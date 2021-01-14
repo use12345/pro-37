86504105401836b88eaa5ad75ee6bfcf1c01a5fe
use12345 <74824012+use12345@users.noreply.github.com>
2021-01-14 12:35:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+var dog,sadDog,happyDog,garden,washroom,bedroom;
+var database;
+var foodS,foodStock;
+var fedTime,lastFed,currentTime;
+var feed,addFood;
+var foodObj;
+var gameState,readState;
+
+
+function preload()
+{
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+garden=loadImage("Garden.png");
+bedroom=loadImage("Bed Room.png");
+washroom=loadImage("Wash Room.png");
+
+
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(400, 500);
+  foodObj = new Food();
+  foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+
+  fedTime = database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed = data.val();
+
+  })
+  readState = database.ref('gameState');
+  readState.on("value",function(data){
+    gameState = data.val();
+
+  })
+
+  dog = createSprite(200,400,150,150);
+  dog.addImage(sadDog);
+  dog.scale = 0.15;
+  feed = createButton("Feed The Dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+  addFood = createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+  
+}
+
+
+function draw() { 
+  currentTime = hour();
+  if(currentTime ===(lastFed+1)){
+    update("Playing");
+    foodObj.garden();
+  }else if(currentTime ===(lastFed+2)){
+    update("sleeping");
+    foodObj.bedroom();
+}
+else if(currentTime>(lastFed+2)&&currentTime<=(lastFed+4)){
+  update("Bathing");
+  foodObj.washroom();
+}
+else{
+  update("Hungry");
+  foodObj.display();
+}
+if(gameState!="Hungry"){
+  feed.hide();
+  addFood.hide();
+  dog.remove();
+}
+else{
+  feed.show();
+  addFood.show();
+  dog.addImage("sadDog");
+}
+drawSprites();
+}
+function readStock(data){
+  foodS = data.val();
+  foodObj.updateFoodStock(foodS);
+
+}
+function feedDog(){
+  dog.addImage(happyDog);
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food: foodObj.getFoodStock(),
+    FeedTime: hour(),
+    gameState: "Hungry"
+
+
+  })
+}
+
+
+
+function addFoods(){
+
+  foodS++;
+  database.ref('/').update({
+    Food: foodS,
+
+  })
+  
+
+}
+function update(state){
+  database.ref('/').update({
+    gameState: state
+  })
+}
